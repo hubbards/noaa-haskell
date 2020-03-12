e009f76e7670d54984d6da7d865b65a5b97ce7e3
Spencer Hubbard <hubbardspencerm@gmail.com>
2020-03-12 16:35:04 -0700

    Update program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 {-| TODO module documentation -}
 module Main where
 
--- TODO add app
+-- NOTE from http-conduit package
+import Network.HTTP.Simple
+  ( Response
+  , httpJSON
+  )
+
+-- NOTE from bytestring package
+import qualified Data.ByteString as B
+
+import Noaa.Request
+import Noaa.Response
+
+-- TODO implement
 main :: IO ()
 main = putStrLn "App not yet implemented"
+
+getDataSets :: B.ByteString -> DataSetsParameters -> IO (Response (Collection DataSet))
+getDataSets token params = httpJSON (dataSetsRequest token params)
